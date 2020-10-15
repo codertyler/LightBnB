57409718594be65363096891dd8ac81a96f447a8
codertyler <codertylerasai@gmail.com>
2020-10-15 18:28:44 +0000

    connected the reservation based on guest_id to the database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -77,7 +77,7 @@ const getUserWithId = function (id) {
       console.log("The error is:", err);
     });
 };
-// exports.getUserWithId = getUserWithId;
+exports.getUserWithId = getUserWithId;
 
 /**
  * Add a new user to the database.
@@ -108,7 +108,13 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT * 
+  FROM properties
+  JOIN reservations
+  ON properties.id = reservations.property_id
+  WHERE guest_id = $1
+  LIMIT $2;`, [guest_id, limit])
+  .then(res => res.rows);
 };
 exports.getAllReservations = getAllReservations;
 
